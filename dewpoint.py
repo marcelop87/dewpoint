4837bc58116427e863e3e15946ded14b5ccff1ad
marcelop87 <126330395+marcelop87@users.noreply.github.com>
2024-02-26 04:13:32 -0400

    Update dewpoint.py

--- dewpoint.py
+++ dewpoint.py
@@ -4,16 +4,12 @@ from joblib import load
 st.title('Calculo Teorico Dew Point')
 st.write('Esta app usa 2 valores de entrada para predecir el Dew Point')
 st.image('newplot.png')
+loaded_model = load('dp_model.joblib')
 
 with st.form("user_inputs"):
     presion = st.number_input('Presion barg', min_value=65)
     temperatura = st.number_input('Temperatura °F', min_value=-22)
-    
-    
-    loaded_model = load('dp_model.joblib')
     nueva_prediccion=loaded_model.predict([[temperatura, presion]])[0]
-    
-    
     submitted = st.form_submit_button("Submit")
     if submitted:
        st.write(f"Se predice que el Dew Point Calculado es de {nueva_prediccion:.2f}")
